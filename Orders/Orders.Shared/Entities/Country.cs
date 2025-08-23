4b2456718a63acfd605beef25a1018845382103a
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2025-08-22 21:44:11 -0700

    Add country entity

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "País")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string Name { get; set; } = null!;
+}
