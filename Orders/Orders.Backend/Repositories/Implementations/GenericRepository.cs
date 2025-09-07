7765437eab12789895e0dee14b3a8edfe9ba4b0c
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2025-09-07 14:13:36 -0700

    complete generic contoller

---
+++ Orders/Orders.Backend/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Orders.Backend.Data;
+using Orders.Backend.Repositories.Interfaces;
+using Orders.Shared.Responses;
+
+namespace Orders.Backend.Repositories.Implementations;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly DataContext _context;
+    private readonly DbSet<T> _entity;
+
+    public GenericRepository(DataContext context)
+    {
+        _context = context;
+        _entity = context.Set<T>();
+    }
+
+    public virtual async Task<ActionResponse<T>> AddAsync(T entity)
+    {
+        _context.Add(entity);
+        try
+        {
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = entity
+            };
+        }
+        catch (DbUpdateException)
+        {
+            return DBUpdateExceptionActionResponse();
+        }
+        catch (Exception exception)
+        {
+            return ExceptionActionResponse(exception);
+        }
+    }
+
+    public virtual async Task<ActionResponse<T>> DeleteAsync(int id)
+    {
+        var row = await _entity.FindAsync(id);
+        if (row == null)
+        {
+            return new ActionResponse<T>
+            {
+                Message = "Registro no encontrado."
+            };
+        }
+        _entity.Remove(row);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = row
+            };
+        }
+        catch
+        {
+            return new ActionResponse<T>
+            {
+                Message = "No se puede borrar, porque tiene registros relacionados."
+            };
+        }
+    }
+
+    public virtual async Task<ActionResponse<T>> GetAsync(int id)
+    {
+        var row = await _entity.FindAsync(id);
+        if (row == null)
+        {
+            return new ActionResponse<T>
+            {
+                Message = "Registro no encontrado."
+            };
+        }
+        return new ActionResponse<T>
+        {
+            WasSuccess = true,
+            Result = row
+        };
+    }
+
+    public virtual async Task<ActionResponse<IEnumerable<T>>> GetAsync()
+    {
+        return new ActionResponse<IEnumerable<T>>
+        {
+            WasSuccess = true,
+            Result = await _entity.ToListAsync()
+        };
+    }
+
+    public virtual async Task<ActionResponse<T>> UpdateAsync(T entity)
+    {
+        _context.Update(entity);
+        try
+        {
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = entity
+            };
+        }
+        catch (DbUpdateException)
+        {
+            return DBUpdateExceptionActionResponse();
+        }
+        catch (Exception exception)
+        {
+            return ExceptionActionResponse(exception);
+        }
+    }
+
+    private ActionResponse<T> ExceptionActionResponse(Exception exception) => new ActionResponse<T>
+    {
+        Message = exception.Message
+    };
+
+    private ActionResponse<T> DBUpdateExceptionActionResponse() => new ActionResponse<T>
+    {
+        Message = "Ya existe el registro."
+    };
+}
