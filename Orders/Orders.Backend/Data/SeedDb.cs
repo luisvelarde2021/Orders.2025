7a382697b6c44a1b1a6c527168fed40e2da1a88b
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2025-09-13 06:38:53 -0700

    Add specific states controller

--- Orders/Orders.Backend/Data/SeedDb.cs
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -32,8 +32,68 @@ public class SeedDb
     {
         if (!_context.Countries.Any())
         {
-            _context.Countries.Add(new Country { Name = "Mexico" });
-            _context.Countries.Add(new Country { Name = "Estados Unidos" });
+            _context.Countries.Add(new Country
+            {
+                Name = "Mexico",
+                States = [
+                new State()
+                {
+                    Name= "Sonora" ,
+                    Cities = [
+                        new City(){Name="Hermosillo" },
+                        new City(){Name="Cd Obregón"},
+                        new City(){Name="Nogales"},
+                        new City(){Name="Navojoa"},
+                        new City(){Name="Guaymas"},
+                        new City(){Name="San Luis Rio Colorado"},
+                        ]
+                },
+                new State()
+                {
+                    Name= "Baja California" ,
+                    Cities = [
+                        new City(){Name="Tijuana" },
+                        new City(){Name="Mexicali"},
+                        new City(){Name="Ensenada"},
+                        new City(){Name="Playas de Rosarito"},
+                        new City(){Name="Tecate"},
+                        new City(){Name="San Felipe"},
+                        new City(){Name="San Quintin"},
+                        ]
+                },
+                ]
+            });
+            _context.Countries.Add(new Country
+            {
+                Name = "Estados Unidos",
+                States = [
+                new State()
+                {
+                    Name= "California" ,
+                    Cities = [
+                        new City(){Name="Los Angeles" },
+                        new City(){Name="San Diego"},
+                        new City(){Name="San Francisco"},
+                        new City(){Name="Sacramento"},
+                        new City(){Name="San Jose"},
+                        new City(){Name="Oakland"},
+                        ]
+                },
+                new State()
+                {
+                    Name= "Texas" ,
+                    Cities = [
+                        new City(){Name="Dallas" },
+                        new City(){Name="Austin"},
+                        new City(){Name="Houston"},
+                        new City(){Name="San Antonio"},
+                        new City(){Name="El Paso"},
+                        new City(){Name="Fort Worth"},
+                        new City(){Name="Midland"},
+                        ]
+                },
+                ]
+            });
             await _context.SaveChangesAsync();
         }
     }
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -19,7 +19,9 @@ builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+builder.Services.AddScoped<IStatesRepository, StatesRepository>();
 builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
+builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
 
 var app = builder.Build();
 SeedData(app);
